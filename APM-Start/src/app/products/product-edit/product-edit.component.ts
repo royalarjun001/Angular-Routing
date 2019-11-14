74330c275b1833841866e3ceb707c7f91d192b82
Arjun kumar verma <arjun.kumar.verma@jci.com>
2019-11-14 06:07:17 +0530

    read route parameters from route using observable

--- APM-Start/src/app/products/product-edit/product-edit.component.ts
+++ APM-Start/src/app/products/product-edit/product-edit.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
 import { MessageService } from '../../messages/message.service';
-
 import { Product } from '../product';
 import { ProductService } from '../product.service';
-import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   templateUrl: './product-edit.component.html',
@@ -19,11 +18,14 @@ export class ProductEditComponent implements OnInit{
 
   constructor(private productService: ProductService,
               private messageService: MessageService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.getProduct(id);
+    this.route.paramMap.subscribe((params) => {
+      const id = +params.get('id');
+      this.getProduct(id);
+    });
   }
 
   getProduct(id: number): void {
@@ -85,5 +87,6 @@ export class ProductEditComponent implements OnInit{
     }
 
     // Navigate back to the product list
+    this.router.navigate(['/products']);
   }
 }

8dbbcaff8fade3e3eb194eb1b3f7481ec5ee68d0
Arjun kumar verma <arjun.kumar.verma@jci.com>
2019-11-17 16:38:43 +0530

    add route gard and use to prevent unauthrized access

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -9,12 +9,14 @@ import { ProductResolver } from './product-resolver.service';
 import { ProductListResolver } from './product-list-resolver.service';
 import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
 import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
+import { AuthGuard } from '../user/auth.guard';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
       { path: 'products',
+        canActivate: [AuthGuard],
         children: [
           {
             path: '',
@@ -24,7 +26,7 @@ import { ProductEditTagsComponent } from './product-edit/product-edit-tags.compo
           {
             path: ':id',
             component: ProductDetailComponent,
-            resolve: { resolvedData: ProductResolver} 
+            resolve: { resolvedData: ProductResolver}
           },
           {
             path: ':id/edit',
